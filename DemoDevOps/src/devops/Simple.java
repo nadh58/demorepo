642cd7ee1ef3655a138b541d795dbc15a3eee294
Anjibabuthota1 <154970661+Anjibabuthota1@users.noreply.github.com>
2024-08-22 10:44:19 +0530

    Update Simple.java

--- DemoDevOps/src/devops/Simple.java
+++ DemoDevOps/src/devops/Simple.java
@@ -1,7 +1,14 @@
 package devops;
 
-public class Simple {
-	public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
-    }
+public class Demo {
+ public static void main(String[] args) {
+	 System.out.println("Hello World");
+	 
+ }
+}
+public class sample{
+ public static void main(String[] args) {
+	 System.out.println("Hello World 2");
+	 
+ }
 }
